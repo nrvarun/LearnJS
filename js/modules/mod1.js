30d011171e7854c6d79e74c002d7251666a386f4
nrvarun <nr.varun79@gmail.com>
2017-01-28 22:34:11 +0530

    a bit changes

--- js/modules/mod1.js
+++ js/modules/mod1.js
@@ -1,8 +1,2 @@
 "use strict";
-
 console.log("Module 1 loaded");
-
-var $  = require('jquery');
-
-$("p").html("Viola damn bro, it works :D");
-console.log($("p").addClass("active"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,91 @@
-var debug = process.env.NODE_ENV !== 'production';
+let debug = process.env.NODE_ENV !== 'production';
 
-var webpack = require('webpack');
-var path = require('path');
+let webpack = require('webpack');
+let path = require('path');
 
 module.exports = {
-    entry: [
-        './js/main.js'
-    ],
-    output: {
-        filename: './js/bundle.js',
-        path: path.resolve(__dirname, './')
-    },
-    watch: true,
-    eslint: {
-        configFile: './.eslintrc'
-    },
+  entry: [
+    './js/main.js'
+  ],
+  output: {
+    filename: './js/bundle.js',
+    path: path.resolve(__dirname, './')
+  },
+  watch: true,
+  eslint: {
+    configFile: './.eslintrc'
+  },
     // more options in the optional jshint object
-    jshint: {
+  jshint: {
         // any jshint option http://www.jshint.com/docs/options/
         // i. e.
-        curly: true,
-        browser: true,
-        camelcase: true,
-        devel: true,
-        node: true,
-        forin: true,
+    curly: true,
+    browser: true,
+    camelcase: true,
+    devel: true,
+    node: true,
+    forin: true,
 
-        eqeqeq: true,
-        eqnull: true,
-        plusplus: true,
+    eqeqeq: true,
+    eqnull: true,
+    plusplus: true,
 
         // jshint errors are displayed by default as warnings
         // set emitErrors to true to display them as errors
-        emitErrors: false,
-        esversion:  6,
+    emitErrors: false,
+    esversion: 6,
 
         // jshint to not interrupt the compilation
         // if you want any file with jshint errors to fail
         // set failOnHint to true
-        failOnHint: false
-    },
+    failOnHint: false
+  },
  	module: {
-         preLoaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'jshint-loader'
+   preLoaders: [
+     {
+       test: /\.js$/,
+       exclude: /node_modules/,
+       loader: 'jshint-loader'
 
-            },
-            {   
-                test: /\.es6$/, 
-                loader: 'eslint-loader', 
-                exclude: /node_modules/
-            }
-        ],
-      loaders: [
-          {
-             test: /\.html$/,
-             exclude: /node_modules/,
-             loader: 'html'
-         },
-         {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                loader: 'style!css!postcss!sass'
-         },
-         {
-                test: /\.es6$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015'] 
-                }
-         }
-      ]
-   },
-   resolve: {
-                extensions: ['', '.js', '.es6']
-    },
-    devServer: {
-        contentBase: './',
-        inline: true,
-        port: 8000
-    },
-    plugins: debug ? [] : [
+     },
+     {
+       test: /\.es6$/,
+       loader: 'eslint-loader',
+       exclude: /node_modules/
+     }
+   ],
+   loaders: [
+     {
+       test: /\.html$/,
+       exclude: /node_modules/,
+       loader: 'html'
+     },
+     {
+       test: /\.scss$/,
+       exclude: /node_modules/,
+       loader: 'style!css!postcss!sass'
+     },
+     {
+       test: /\.es6$/,
+       exclude: /node_modules/,
+       loader: 'babel-loader',
+       query: {
+            presets: ['react', 'es2015']
+          }
+     }
+   ]
+ },
+  resolve: {
+    extensions: ['', '.js', '.es6']
+  },
+  devServer: {
+    contentBase: './',
+    inline: true,
+    port: 8000
+  },
+  plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
   ]
 };

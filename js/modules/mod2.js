4a563bfe2755d93f84f52bcfdea0a567bb4fb728
nrvarun <nr.varun79@gmail.com>
2017-01-29 02:26:19 +0530

    a few changes

--- js/modules/mod2.js
+++ js/modules/mod2.js
@@ -1,13 +1,15 @@
-console.log("Module 2 loaded");
+'use strict';
+
+console.log('Module 2 loaded');
 
 const obj = { };
 
-[obj.a,obj.b] = ["varun","26"];
+[obj.a, obj.b] = ['varun', '26'];
 
 console.log(obj);
 
-var add = function(a,b) {
-    return a+b;
+let add = function (a, b) {
+  return a + b;
 };
 
-console.log(add(10,22));
+console.log('sum is '+add(10, 22));
